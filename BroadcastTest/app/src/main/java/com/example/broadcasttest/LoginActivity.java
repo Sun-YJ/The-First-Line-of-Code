4c329e3ee4e87dec67ebb5f854018042fa3ec230
Yaojia Sun <Yaojia Sun>
2017-03-01 18:35:42 +0800

    add a function of remembering password

--- BroadcastTest/app/src/main/java/com/example/broadcasttest/LoginActivity.java
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.broadcasttest;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,17 +16,34 @@ import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
 
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+    private CheckBox rememberPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
         login = (Button) findViewById(R.id.login);
+
+        boolean isRemember = pref.getBoolean("remember_password",false);
+        if(isRemember){
+            //将账号和密码都设置到文本框
+            String account = pref.getString("account","");
+            String password = pref.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +51,15 @@ public class LoginActivity extends BaseActivity {
                 String password = passwordEdit.getText().toString();
 
                 if(account.equals("admin") && password.equals("123456")){
+                    editor = pref.edit();
+                    if(rememberPass.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.commit();
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
--- BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.broadcasttest;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends BaseActivity {
@@ -23,4 +29,5 @@ public class MainActivity extends BaseActivity {
             }
         });
     }
+
 }
